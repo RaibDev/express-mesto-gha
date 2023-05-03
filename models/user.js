9410148c18a5fa709724972bdb63cba850bd6eb1
Vladimir Dubowitskiy <vladimir.dubowitskiy@gmail.com>
2023-05-03 23:14:25 +0300

    refactor: bugfix for review

--- models/user.js
+++ models/user.js
@@ -17,15 +17,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 8,
-  },
+  // email: {
+  //   type: String,
+  //   required: true,
+  //   unique: true,
+  // },
+  // password: {
+  //   type: String,
+  //   required: true,
+  //   minLength: 8,
+  // },
 });
 module.exports = mongoose.model('user', userSchema);

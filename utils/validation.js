44f86b9b675319acd4ffca1de12755795d8a2ac1
Vladimir Dubowitskiy <vladimir.dubowitskiy@gmail.com>
2023-05-11 19:07:25 +0300

    feature: add regExp

--- utils/validation.js
+++ utils/validation.js
@@ -1,26 +1,26 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { Joi } = require('celebrate');
-const { regexUrl } = require('./constants');
+// const { regexUrl } = require('./constants');
 
 const createUserValidation = {
   body: Joi.object().keys({
-    email: Joi.string().email().required().message({
+    email: Joi.string().email().required().messages({
       'string.email': 'Введён некорректный email адрес',
       'any.required': 'Поле email не должно быть пустым',
     }),
-    password: Joi.string().min(8).required().message({
+    password: Joi.string().min(8).required().messages({
       'string.min': 'Пароль должен содержать не менее 8 символов',
       'any.required': 'Поле пароля не должно быть пустым',
     }),
-    name: Joi.string().min(2).max(30).message({
+    name: Joi.string().min(2).max(30).messages({
       'string.email': 'Введён некорректный email адрес',
       'any.required': 'Поле email не должно быть пустым',
     }),
-    about: Joi.string().min(2).max(30).message({
+    about: Joi.string().min(2).max(30).messages({
       'string.min': 'Пароль должен содержать не менее 8 символов',
       'any.required': 'Поле пароля не должно быть пустым',
     }),
-    avatar: Joi.string().pattern(regexUrl).message('Ссылка на аватар введёна некорректно'),
+    avatar: Joi.string().pattern(/^(ftp|http|https):\/\/[^ "]+$/).message('Ссылка на аватар введёна некорректно'),
   }),
 };
 
@@ -45,11 +45,11 @@ const getUserValidation = {
 
 const updateUserValidation = {
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).message({
+    name: Joi.string().min(2).max(30).messages({
       'string.min': 'Поле "имя" должно содержать более 2х символов',
       'string.max': 'Поле "имя" не должно содержать более 30 знаков',
     }),
-    about: Joi.string().min(2).max(30).message({
+    about: Joi.string().min(2).max(30).messages({
       'string.min': 'Поле "сфера занятий" должно содержать более 2х символов',
       'string.max': 'Поле "сфера занятий" не должно содержать более 30 знаков',
     }),
@@ -58,7 +58,7 @@ const updateUserValidation = {
 
 const updateAvatarValidation = {
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(regexUrl).message('Ссылка на аватар введёна некорректно'),
+    avatar: Joi.string().pattern(/^(ftp|http|https):\/\/[^ "]+$/).message('Ссылка на аватар введёна некорректно'),
   }),
 };
 
@@ -68,12 +68,12 @@ const createCardValidation = {
       .min(2)
       .max(30)
       .required()
-      .message({
+      .messages({
         'string.min': 'Поле "Название места" должно содержать более 2х символов',
         'string.max': 'Поле "Название места" не должно содержать более 30 знаков',
         'any.required': 'Поле "Название места" не должно быть пустым',
       }),
-    link: Joi.string().pattern(regexUrl).required().message({
+    link: Joi.string().pattern(/^(ftp|http|https):\/\/[^ "]+$/).required().messages({
       'string.dataUri': 'Введена некорректная ссылка на картинку места',
       'any.required': 'Поле ссылки не должно быть пустым',
     }),

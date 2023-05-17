145de0effa353ce684f002b5d6b347a593f734a3
Vladimir Dubowitskiy <vladimir.dubowitskiy@gmail.com>
2023-05-17 18:30:41 +0300

    bugfix: auth-token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,14 @@ const { SECRET_KEY } = require('../utils/constants');
 const customErrors = require('../utils/errors/index');
 
 module.exports = (req, res, next) => {
-  const { autorization } = req.headers;
+  const { authorization } = req.headers;
 
-  if (!autorization || autorization.startsWith('Bearer')) {
+  if (!authorization || authorization.startsWith('Bearer')) {
     throw new customErrors.Unautorized('Вы не авторизовались');
     // res.status(401).send({ message: 'Вы не авторизовались' });
     // return;
   }
-  const token = autorization.replace('Bearer ', '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, SECRET_KEY);
